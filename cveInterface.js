223466dad6c716626ec93b6129254b9f6572a82e
John Stroud <jpstroud@sei.cmu.edu>
2023-02-14 16:27:33 -0500

    Skip method test

--- cveInterface.js
+++ cveInterface.js
@@ -225,6 +225,14 @@ async function skip() {
 	    $('#loginModal').modal('hide');
 	    show_cve_table();
 	}, 2300);
+	if ($('#storeLocal').is(':checked')) {
+	    store = localStorage;
+	} else {
+	    store = sessionStorage;
+	}
+	$('#loginModal .form-control').each(function(_,x) {
+	    store.setItem(store_tag+$(x).attr('id'),$(x).val());
+	});
 }
     
 async function login() {
